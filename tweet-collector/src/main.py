74e3629590b46db3cbf1a8f44f48764d09fc86f6
said <saidmounjisam@gmail.com>
2024-12-08 11:05:00 +0100

    Add main entry point for collector

---
+++ tweet-collector/src/main.py
@@ -0,0 +1,9 @@
+import logging
+from collector import TweetCollector
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+logger = logging.getLogger(__name__)
+
+if __name__ == "__main__":
+    collector = TweetCollector()
+    collector.run()
